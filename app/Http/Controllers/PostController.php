f71192c6570094dee08db800e4c3cc0ff7000ae6
Gun Gun Priatna <gungunpriatna02@gmail.com>
2023-02-20 20:23:50 +0700

    init repo

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Str;
+
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): \Illuminate\Contracts\View\View
+    {
+        $posts = Post::orderBy('id', 'DESC')->paginate(10);
+        return view('posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): \Illuminate\Contracts\View\View
+    {
+        return view('posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $this->validate($request, [
+            'title' => 'required|string|max:151',
+            'content' => 'required',
+            'status' => 'required|integer'
+        ]);
+
+        $post = Post::create([
+            'title' => $request->get('title'),
+            'content' => $request->get('content'),
+            'status' => $request->get('status'),
+            'slug' => Str::slug($request->get('title'))
+        ]);
+
+        return redirect()->route('post.index')
+            ->with('success', 'Post created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post): Response
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Post $post): \Illuminate\Contracts\View\View
+    {
+        return view('posts.edit', compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post): RedirectResponse
+    {
+        $this->validate($request, [
+            'title' => 'required|string|max:151',
+            'content' => 'required',
+            'status' => 'required|integer'
+        ]);
+
+        $post->update([
+            'title' => $request->get('title'),
+            'content' => $request->get('content'),
+            'status' => $request->get('status'),
+            'slug' => Str::slug($request->get('title'))
+        ]);
+
+        return redirect()->route('post.index')
+            ->with('success', 'Post updated successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post): RedirectResponse
+    {
+        $post->delete();
+        return redirect()
+            ->route('post.index')
+            ->with('success', 'Post deleted successfully.');
+    }
+}
